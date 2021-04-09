3c5be34d02108d73965acf63dc3752f0824fcf50
Ahmed-Aldallee <77866261+Ahmed-Aldallee@users.noreply.github.com>
2021-04-09 14:49:53 -0600

    a

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -40,7 +40,7 @@ class MovieList extends Component {
                         <Carousel.Item key={movie.title}>
                             <div>
                                 <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
-                                    <Nav.Link><Image className="image" src={movie.ImageUrl} thumbnail /></Nav.Link>
+                                    <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
                                 </LinkContainer>
                             </div>
                             <Carousel.Caption>
