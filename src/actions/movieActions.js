bd9165316d143cc6ce934555ba4ed61af20ea3d2
Ahmed-Aldallee <77866261+Ahmed-Aldallee@users.noreply.github.com>
2021-04-09 15:55:16 -0600

    a

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -32,7 +32,7 @@ export function setMovie(movie) {
 export function fetchMovie(title) {
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}movie/${title}?reviews=true`, {
+        return fetch(`${env.REACT_APP_API_URL}/movie/${title}?reviews=true`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -54,7 +54,7 @@ export function fetchMovie(title) {
 export function fetchMovies() {
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}movie?reviews=true`, {
+        return fetch(`${env.REACT_APP_API_URL}/movie?reviews=true`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
