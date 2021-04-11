0e1e2da1c4ecac34ec8c89fb3cc43bf89815371b
Ahmed-Aldallee <77866261+Ahmed-Aldallee@users.noreply.github.com>
2021-04-10 18:28:12 -0600

    a

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -72,3 +72,28 @@ export function fetchMovies() {
         }).catch((e) => console.log(e));
     }
 }
+
+export function submitReview(review, title){
+    const env = runtimeEnv();
+    return dispatch => {
+        review.rating = parseInt(review.rating);
+        return fetch(`${env.REACT_APP_API_URL}/review/`, {
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify(review),
+            mode: 'cors'})
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            }).then(() =>{
+                dispatch(fetchMovie(title));
+            })
+            .catch((e)=> console.log(e));
+    }
+}
